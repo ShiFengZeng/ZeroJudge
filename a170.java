515f5b4c1697a64552d488c10ee7758ae87102d6
ShiFengZeng <90011551a90011551@yahoo.com.tw>
2018-09-02 14:12:40 +0800

    2018/09/02

--- a170.java
+++ a170.java
@@ -1,31 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class a170 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		int n;
 		String a, b;
 
 		long sum;
 		String hex;
 
-		while (scan.hasNext()) {
-			n = scan.nextInt();
+		String line;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(System.out);
+		while ((line = br.readLine()) != null) {
+			n = Integer.valueOf(line);
 
 			for (int i = 0; i < n; ++i) {
-				a = scan.next();
-				b = scan.next();
+				String[] inp = br.readLine().split(" ");
+				a = inp[0];
+				b = inp[1];
 
 				sum = Integer.valueOf(a, 8) + Integer.valueOf(b, 8);
 
 				hex = Long.toHexString(sum);
 				hex = hex.toUpperCase();
-				System.out.println(hex);
+				pw.println(hex);
 
 			}
 
 		}
-		scan.close();
+		pw.close();
 	}
 }
